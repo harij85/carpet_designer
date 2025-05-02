16dde0ef7dacbb85c14ef0b549a11e547adee241
Harish Jariwala <info@electronicmusicwales.com>
2025-05-02 11:22:57 +0100

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,27 @@
 // static/js/main.js
 
 // SVG icons for lock states
-const LOCK_CLOSED_SVG = '<svg …></svg>';
-const LOCK_OPEN_SVG   = '<svg …></svg>';
+const LOCK_CLOSED_SVG = `
+<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none"
+     viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round"
+        d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75
+           m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25
+           v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75
+           a2.25 2.25 0 0 0-2.25 2.25v6.75
+           a2.25 2.25 0 0 0 2.25 2.25Z" />
+</svg>`;
+
+const LOCK_OPEN_SVG = `
+<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none"
+     viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round"
+        d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75
+           M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25
+           v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75
+           a2.25 2.25 0 0 0-2.25 2.25v6.75
+           a2.25 2.25 0 0 0 2.25 2.25Z" />
+</svg>`;
 
 // Configuration
 const BASE_TILE_SIZE = 240;
